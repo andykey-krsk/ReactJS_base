7891f72e695ca21f479de52a72b68d3c6a12ddaa
andykey-krsk <andykey@bk.ru>
2021-08-15 22:44:00 +0700

    try themes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import { ThemeProvider, createTheme } from "@material-ui/core"
+import { createTheme } from "@material-ui/core"
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
-import { Home, Profile } from "./components"
+import { Home, Profile, DefaultThemeProvider } from "./components"
 import { Chat } from "./pages"
 import "./styles/app.scss"
 
-const theme = createTheme({
-  dark: {
-    color: "$dark",
-  },
-  light: {
-    color: "$light",
-  },
-})
+const themes = {
+  dark: createTheme({
+    color: "#000",
+  }),
+  light: createTheme({
+    color: "#fff",
+  }),
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
+      <DefaultThemeProvider themes={themes} initialTheme="light">
         <Switch>
           <Route path="/" exact={true} component={() => <Home />} />
           <Route path="/profile" component={() => <Profile />} />
           <Route path="/chat" component={() => <Chat />} />
           <Route path="*" component={() => <h1>404</h1>} />
         </Switch>
-      </ThemeProvider>
+      </DefaultThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 
