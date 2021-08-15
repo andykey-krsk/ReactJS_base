9b8ce59b978b6cc81cd0599082e09eec676ef02e
andykey-krsk <andykey@bk.ru>
2021-08-16 01:36:17 +0700

    lesson_5. install redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { createTheme } from "@material-ui/core"
 import React from "react"
 import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import { Home, Profile, DefaultThemeProvider } from "./components"
 import { Chat } from "./pages"
+import { store } from "./store"
 import "./styles/app.scss"
 
 const themes = {
@@ -17,16 +19,18 @@ const themes = {
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <DefaultThemeProvider themes={themes} initialTheme="light">
-        <Switch>
-          <Route path="/" exact={true} component={() => <Home />} />
-          <Route path="/profile" component={() => <Profile />} />
-          <Route path="/chat" component={() => <Chat />} />
-          <Route path="*" component={() => <h1>404</h1>} />
-        </Switch>
-      </DefaultThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <DefaultThemeProvider themes={themes} initialTheme="light">
+          <Switch>
+            <Route path="/" exact={true} component={() => <Home />} />
+            <Route path="/profile" component={() => <Profile />} />
+            <Route path="/chat" component={() => <Chat />} />
+            <Route path="*" component={() => <h1>404</h1>} />
+          </Switch>
+        </DefaultThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 
   document.getElementById("root")
